e74bc0ea7b3b14395f11b86fba6d7b4a3154c226
Brian He <brian.he17@gmail.com>
2020-02-02 23:13:59 -0500

    access media urls

--- twitter_api.py
+++ twitter_api.py
@@ -9,6 +9,20 @@ auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
+# Print Twitter Feed of a User
+public_tweets = api.user_timeline(screen_name="FloodSocial", count=200)
+
+# Images
+media_files = set()
+
 for tweet in public_tweets:
     print(tweet.text)
+    media = tweet.entities.get('media', [])
+    if(len(media) > 0):
+        media_files.add(media[0]['media_url'])
+
+print('---------------------------------')
+
+# Print all media
+for media in media_files:
+    print(media)
