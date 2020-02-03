784f9d427e6292a7f10ef64f72e752021328226a
Brian He <brian.he17@gmail.com>
2020-02-03 01:19:23 -0500

    temp fix

---
+++ user.py
@@ -0,0 +1,11 @@
+import google_twitter_api 
+
+
+google_twitter_api.api_print("FloodSocial",10)
+
+#TODO 
+#eNcaPsuLaTiOn
+
+# 1. Instead of printing tweets, change to return as json
+# 2. Incorporate Google API on twitter images
+# 3. Develop API function calls
