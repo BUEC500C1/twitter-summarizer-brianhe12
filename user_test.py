499cdfc8e9cb4588ff49efff7225146b5f898615
Brian He <brian.he17@gmail.com>
2020-02-03 01:33:32 -0500

    changed to tweepy

--- user_test.py
+++ user_test.py
@@ -2,7 +2,7 @@ import google_twitter_api
 
 def test():
     try:
-        google_twitter_api.api_print("FloodSocial",10)
+        import tweepy
         assert True
     except:
         assert False
