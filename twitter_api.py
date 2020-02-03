1507d6227de755e9c2207bcac2f6324b9e50ad21
Brian He <brian.he17@gmail.com>
2020-02-02 23:28:16 -0500

    testing media

--- twitter_api.py
+++ twitter_api.py
@@ -10,18 +10,19 @@ auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"
 api = tweepy.API(auth)
 
 # Print Twitter Feed of a User
-public_tweets = api.user_timeline(screen_name="FloodSocial", count=200)
+public_tweets = api.user_timeline(screen_name="FloodSocial", count=50)
 
 # Images
 media_files = set()
 
 for tweet in public_tweets:
     print(tweet.text)
+    print('\n')
     media = tweet.entities.get('media', [])
     if(len(media) > 0):
         media_files.add(media[0]['media_url'])
 
-print('---------------------------------')
+print('---------------------------------\n')
 
 # Print all media
 for media in media_files:
